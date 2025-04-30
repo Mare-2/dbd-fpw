35106127ec7498ca097e909007b304380aa21253
Chiara Didu <chiaradidu04@gmail.com>
2025-04-30 16:12:26 +0200

    aggiornamento sezioni home

--- dbd-project/src/views/HomeView.vue
+++ dbd-project/src/views/HomeView.vue
@@ -6,25 +6,52 @@
   <main class="col-6">
     <!-- Viene presentata l'attività -->
 
-    <h1>Debugging Ducks</h1>
+    <h1>CHI SIAMO</h1>
+    
 
     <article>
-      <div>
-        <h2>Benvenuti su questo sito!</h2>
-        
+      <div>      
         <p>
-          <b>Benvenuti nel mondo delle paperelle!</b> Scopri la nostra collezione di paperelle uniche, divertenti e colorate. 
+
+          <h3><b>Benvenuti nel mondo delle paperelle!</b></h3>
+          Scopri la nostra collezione di paperelle uniche, divertenti e colorate. 
           Che tu stia affrontando un bug ostinato, cercando una soluzione creativa o migliorando il tuo flusso di lavoro, qui 
           troverai la paperella perfetta per il debugging!
-          <b>Cosa Offriamo:</b>
+          <br><br>
+        </p>
+        
+        <!--aggiungiamo i link alle parti della home-->
+        <ul>
+          <li><b>La nostra storia</b></li>
+          <li><b>Cosa offriamo?</b></li>
+          <li><b>I nostri valori</b></li>
+          <li><b>Scopri il nostro mondo</b></li>
+        </ul>
+
+        <p>
+          <h3>La nostra storia</h3>
+          da scrivere
+        </p>
+
+        <p>
+          <h3><b>Cosa Offriamo?</b></h3>
         </p>
         <ul>
           <li><b>Paperelle per tutti i gusti:</b> dalle paperelle classiche alle edizioni limitate.</li>
           <li><b>Accessori:</b> tutto ciò che serve per rendere le tue paperelle ancora più speciali.</li>
           <li><b>Esperienza unica:</b> un negozio pieno di allegria e creatività, dedicato agli amanti delle paperelle.</li>
         </ul>
+
         <p>
-          <b>Scopri il Nostro Mondo</b> <br>Esplora le nostre collezioni e lasciati stupire dal design originale e dalla qualità dei nostri prodotti. Qui, le paperelle non sono solo oggetti, ma vere protagoniste di momenti indimenticabili!
+          <br>
+          <h3>I nostri valori</h3>
+          da scrivere, ci teniamo all'ecologia, plastica bella
+        </p>
+
+        <p>
+          <br>
+          <h3><b>Scopri il Nostro Mondo</b></h3>
+          Esplora le nostre collezioni e lasciati stupire dal design originale e dalla qualità dei nostri prodotti. Qui, le paperelle non sono solo oggetti, ma vere protagoniste di momenti indimenticabili!
           Vieni a Trovarci! Siamo entusiasti di condividere con te la nostra passione per le paperelle. Facci sapere se possiamo aiutarti a scegliere la tua preferita.
         </p>
       </div>
