a0038e0a55f92b87831a0d149752d1dfcaeffbdc
Flaviana Ruiu <flavy.ruiu@gmail.com>
2025-04-30 14:29:30 +0200

    header: logo a sinistra desktop

--- dbd-project/src/components/Header.vue
+++ dbd-project/src/components/Header.vue
@@ -4,11 +4,43 @@
 <template>
     <!-- logo immagine e titolo -->
     <header class="col-12">
-        <RouterLink to="/"> <img src="../assets/img/logo.png" alt="Logo di dbd" width="200"> </RouterLink>
+        <RouterLink to="/"> <img id="logo_header" src="../assets/img/logo.png" alt="Logo di dbd" width="100"> </RouterLink>
+    
+        <h1 id="titolo_header">Debugging Ducks</h1>
         <NavBar></NavBar>
     </header>
 </template>
 
 <style>
 
+    @media only screen and (min-width: 768px){
+    
+        #titolo_header{
+            text-align: left;
+            color : var(--white);
+            font-size: 30px;
+            padding-top:10px;
+        }
+
+        #logo_header{
+            float: left;
+            padding: 20px;
+        }
+    
+    }
+
+    @media only screen and (max-width: 768px){
+        #titolo_header{
+            text-align: center;
+            color : var(--white);
+            font-size: 40px;
+            
+        }
+
+        #logo_header{
+            float: center;
+            padding-top: 15px;
+        }
+        
+    }
 </style>
